7b69766ff14cacf8f11c0c563b91779bfb962957
Gung Nanda <gungnands@gmail.com>
2026-01-20 02:02:07 +0800

    refactor main file

---
+++ index.js
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = require("./src/app");
+const db = require("./src/config/db");
+
+const PORT = process.env.PORT;
+
+(async () => {
+  try {
+    await db.query("SELECT 1");
+    console.log("MySQL connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+    process.exit(1);
+  }
+})();
--- src/app.js
+++ src/app.js
@@ -2,28 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const dotenv = require("dotenv");
 
-dotenv.config();
-// Initialize Express App
-const app = express();
-const db = require("./config/db");
-const PORT = process.env.PORT || 5000;
-// init routes
+// routes
 const authRoutes = require("./routes/auth.routes");
 const roomRoutes = require("./routes/room.routes");
 const tenantRoutes = require("./routes/tenant.routes");
+
+const app = express();
+
 // Middleware
 app.use(helmet());
-
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
@@ -35,22 +30,9 @@ app.get("/", (req, res) => {
     message: "Welcome to Coasther API",
   });
 });
+
 app.use("/api/auth", authRoutes);
 app.use("/api/rooms", roomRoutes);
 app.use("/api/tenants", tenantRoutes);
-// Start the server after verifying DB connection
-(async () => {
-  try {
-    await db.query("SELECT 1");
-    console.log("MySQL connected");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error("Database connection failed:", error.message);
-    process.exit(1);
-  }
-})();
 
 module.exports = app;
