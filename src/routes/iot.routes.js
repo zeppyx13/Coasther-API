d00390ce4f62fd340adcd19106d0a6224199e5ec
Gung Nanda <gungnands@gmail.com>
2026-04-12 13:44:13 +0800

    auth routes

--- src/routes/iot.routes.js
+++ src/routes/iot.routes.js
@@ -7,12 +7,25 @@ const iotAuth = require("../middlewares/iotAuth.middleware");
 const iotController = require("../controllers/iot.controller");
 
 router.post("/meter-reading", iotAuth, iotController.meterReading);
-router.get("/live-status", iotController.getAllLiveStatus);
-router.get("/live-status/:roomId", iotController.getLiveStatusByRoomId);
+
+router.get(
+  "/live-status",
+  auth,
+  requireRole(["admin", "manager"]),
+  iotController.getAllLiveStatus,
+);
+router.get(
+  "/live-status/:roomId",
+  auth,
+  requireRole(["admin", "manager", "tenant"]),
+  iotController.getLiveStatusByRoomId,
+);
+
 router.post(
   "/relay/:roomId",
   auth,
   requireRole(["admin", "manager"]),
   iotController.relayControl,
 );
+
 module.exports = router;
--- src/routes/weather.routes.js
+++ src/routes/weather.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const weatherController = require("../controllers/weather.controller");
+const auth = require("../middlewares/auth.middleware");
 
-router.get("/current", weatherController.getCurrentWeather);
+router.get("/current", auth, weatherController.getCurrentWeather);
 
 module.exports = router;
